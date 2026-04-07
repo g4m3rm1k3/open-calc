8eb9ff8207a4eba6546472419dfeb9bdc211244b
g4m3rm1k3 <g4m3rm1k3@hotmail.com>
2026-04-07 05:55:19 -0400

    Implement structure for code changes with placeholders for future updates

--- src/content/chemistry-2/index.js
+++ src/content/chemistry-2/index.js
@@ -1,4 +1,6 @@
 import lesson2_0 from './lesson2-0.js'
+import lesson2_1 from './lesson2-1.js'
+import lesson2_2 from './lesson2-2.js'
 
 const CHEM_CH2 = {
   title: 'Chemical Bonding',
@@ -6,7 +8,7 @@ const CHEM_CH2 = {
   slug: 'chem-chemical-bonding',
   description: 'Discover why atoms bond, the difference between ionic and covalent bonds, and how electronegativity determines bond type.',
   course: 'chemistry-1',
-  lessons: [lesson2_0],
+  lessons: [lesson2_0, lesson2_1, lesson2_2],
 };
 
 export default [CHEM_CH2];
