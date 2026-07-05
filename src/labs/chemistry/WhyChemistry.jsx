e84181c5f4b120bdca10975e1923a8171889fbec
g4m3rm1k3 <g4m3rm1k3@hotmail.com>
2026-07-05 11:30:40 -0400

    Chemistry fixes

--- src/labs/chemistry/WhyChemistry.jsx
+++
@@ -1,6 +0,0 @@
-import ScienceNotebook from '../../courses/chemistry/viz/ScienceNotebook.jsx'
-import { LESSON_CHEM_1_0 } from '../../../courses/chemistry/1-elements-atomic-structure/001-lesson1-0.js'
-
-export default function WhyChemistry({ params }) {
-  return <ScienceNotebook lesson={LESSON_CHEM_1_0} params={params} />
-}
